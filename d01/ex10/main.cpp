33ec5590fe53f1ffd26d9c16cf8de51b304b1877
etugoluk <chihyahya98@gmail.com>
2018-09-29 10:35:41 +0300

    ex10 - not full

--- d01/ex10/main.cpp
+++ d01/ex10/main.cpp
@@ -1,12 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sys/types.h>
 
-void files()
+void files(int argc, char **argv)
 {
+	std::ifstream is;
+	std::string s;
+	// struct stat status;
 
+	for (int i = 1; i < argc; i++)
+	{
+		is.open(argv[i], std::ifstream::in);
+		if (!is.is_open())
+		{
+			std::cout << "cat: " << argv[i] << ": No such file or directory" << std::endl;
+			continue;
+		}
+		while (std::getline(is, s))
+		{
+			std::cout << s << std::endl;
+		}
+		is.close();
+	}
 }
 
 void console()
 {
-	
+	std::string s;
+
+	while (std::getline(std::cin, s))
+	{
+		std::cout << s << std::endl;
+	}
 }
 
 int main(int argc, char **argv)
